c975a70e186bf99814f9a2adf8b751a3d3ca420c
Jacob Wisniewski <jacobawisniewski@gmail.com>
2023-12-06 17:06:26 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DayOneComponent } from './day-one/day-one.component';
+import { DayTwoComponent } from './day-two/day-two.component';
+import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'test', component: TestComponent },
+    { path: 'day1', component: DayOneComponent },
+    { path: 'day2', component: DayTwoComponent },];
